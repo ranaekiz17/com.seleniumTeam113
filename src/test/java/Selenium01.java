d737b306be52656b54263bf7e605b114a764a1ff
ranaekiz17 <ekizgrana94@gmail.com>
2023-05-04 22:11:38 +0200

    4. dersin sonu

--- src/test/java/Selenium01.java
+++ src/test/java/Selenium01.java
@@ -3,16 +3,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Set;
 
 public class Selenium01 {
 
-    public static void main(String[] args) {
-        System.setProperty("Webdriver.chrome.driver","src/resources/chromedriver.exe");
 
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.amazon.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+
+        driver.get("https://www.automationexercise.com/");
+
+        List<WebElement> markaElementlist = driver.findElements(By.className("brands_product"));
+
+
+
+        WebElement productElementi = driver.findElement(By.partialLinkText("Products"));
+        productElementi.click();
+        WebElement aramaKutusuElementi = driver.findElement(By.id("search_product"));
+        aramaKutusuElementi.sendKeys("TSHIRT");
+        aramaKutusuElementi.click();
+        WebElement butonElementi = driver.findElement(By.id("submit_search"));
+        butonElementi.click();
+
+        // WebElement cartElementi = driver.findElement(By.className("choose"));
+        //cartElementi.click();
+
+
+
+
+        Thread.sleep(3000);
+        driver.close();
 
     }
 }
---
+++ src/test/java/day04_xpath_cssSelector/C03_Xpath.java
@@ -0,0 +1,54 @@
+package day04_xpath_cssSelector;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C03_Xpath {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+        // 1- https://the-internet.herokuapp.com/add_remove_elements/ adresine gidin
+        driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
+
+        Thread.sleep(3000);
+        //2- Add Element butonuna basin
+        // driver.findElement(By.xpath("//*[text()=‘Add Element’]")).click();
+        driver.findElement(By.xpath("//*[text()='Add Element']")).click();
+
+
+        // 3- Delete butonu’nun gorunur oldugunu test edin
+        WebElement deleteButtonElementi= driver.findElement(By.xpath("//*[text()='Delete']"));
+
+        //WebElement deleteButonuElementi = driver.findElement(By.xpath("//button[@class=‘added-manually’]"));
+        if (deleteButtonElementi.isDisplayed()){
+            System.out.println("Delete buton testi  PASSED");
+        }else {
+            System.out.println("Delete buton testi  FAILED");
+        }
+        Thread.sleep(3000);
+        // 4- Delete tusuna basin
+        deleteButtonElementi.click();
+
+        // 5- “Add/Remove Elements” yazisinin gorunur oldugunu test edin
+        WebElement addElementsElementi = driver.findElement(By.xpath("//h3"));
+
+
+        if (addElementsElementi.isDisplayed()){
+            System.out.println("Add/Remove  testi  PASSED");
+        }else {
+            System.out.println("Add/Remove testi  FAILED");
+        }
+
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
